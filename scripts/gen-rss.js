2f0dd3fd592f91d6f850794a85cc30fd4aa179ac
Joseph Wasden <joseph.wasden@gmail.com>
2024-01-10 20:13:57 -0800

    add First template

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml',
+    title: 'Kearston Galindo',
+    site_url: 'https://kearstongalindo.com',
+    feed_url: 'https://kearstongalindo.com/feed.xml',
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
